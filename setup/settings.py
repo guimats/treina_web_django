3e457446852494c6da7b3b766c081d628f45fabe
guimats <guilherme.mat.ds@gmail.com>
2024-06-21 11:18:36 -0300

    Criando models, realizando migrations e isolando informações confidenciais do código

--- setup/settings.py
+++ setup/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config, Csv  # type: ignore
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#%@0$#7=hdrjs!x$)-(lbep&l_d&m^d$^$cg6ry!esc-c+hp-7'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = []  # type: ignore
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv)  # type: ignore
 
 
 # Application definition
