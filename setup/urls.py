f60cb06d62bf9e23a7451180e9264880db2133d2
guimats <guilherme.mat.ds@gmail.com>
2024-06-25 13:21:08 -0300

    Criando UPDATE_VIEW

--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin  # type: ignore
 from django.urls import path  # type: ignore
-from todos.views import TodoListView, TodoCreateView
+from todos.views import TodoListView, TodoCreateView, TodoUpdateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TodoListView.as_view(), name='todo_list'),
     path('create/', TodoCreateView.as_view(), name='todo_create'),
+    path('update/<int:pk>', TodoUpdateView.as_view(), name='todo_update'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, CreateView  # type: ignore
+from django.views.generic import (  # type: ignore
+    ListView, CreateView, UpdateView)
 from django.urls import reverse_lazy  # type: ignore
 from .models import Todo
 
@@ -8,6 +9,12 @@ class TodoListView(ListView):
 
 
 class TodoCreateView(CreateView):
+    model = Todo
+    fields = ['title', 'deadline']
+    success_url = reverse_lazy('todo_list')
+
+
+class TodoUpdateView(UpdateView):
     model = Todo
     fields = ['title', 'deadline']
     success_url = reverse_lazy('todo_list')
